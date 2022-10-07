20e653c57dd8268cb79bdc7851fe887da7842647
Noelani West <noelani.rose14@gmail.com>
2022-10-06 22:32:57 -0500

    finish add btn

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,67 @@ $(document).ready(onReady);
 function onReady() {
     $('.addBtn').on('click', addTask);
 
+    getTask();
 }
 
+let completed = 'FALSE';
 function addTask(){
     console.log('in add task function');
+
+    let taskToAdd = {
+        task: $('.textInput').val(),
+        completed: completed
+    }
+
+    $.ajax({
+        method: 'POST',
+        url: '/to-do/',
+        data: taskToAdd
+    })
+        .then(function(response){
+            console.log('task added...');
+
+            $('.textInput').val(''),
+            getTask();
+        })
+        .catch((err) => {
+            console.log('posting new task error', err);
+        });
+}
+
+function getTask(){
+    console.log('getting task request');
+
+    $.ajax({
+        method: 'GET',
+        url: '/to-do'
+    })
+    .then((response) => {
+        console.log('getting task response from server', response);
+        const listOfTasks = response;
+
+        render(response);
+    })
+    .catch((err) => {
+        console.log('error in getting task response...', err);
+
+    })
+} // end of getTask
+
+
+
+
+
+
+
+
+function render(listOfTasks){
+    $('#taskTable').empty();
+    for(let task of listOfTasks){
+        $('#taskTable').append(`
+        <ul>
+            <li>${task.task}<button id = "deleteBtn" data-id = ${task.id}>Delete</button></li>
+        </ul>`)
+    }
+
 }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,44 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
+router.get('/', (req, res) => {
+
+    pool.query(`SELECT * FROM "todo";`)
+
+        .then((dbRes) => {
+        console.log(dbRes.rows)
+            res.send(dbRes.rows)
+        })
+        .catch ((err) => {
+            console.log('getting db response failed...', err);
+            res.sendStatus(500);
+        })
+});
+
+
+router.post('/', (req, res) => {
+    console.log('posting incoming task...', req.body);
+    let newTask = req.body
+
+    const sqlText = `
+    INSERT INTO "todo" ("task", "complete")
+    VALUES ($1, $2);
+    `;
+
+    const sqlParams = [
+        req.body.task,
+        req.body.completed
+    ];
+
+    pool.query(sqlText, sqlParams)
+        .then((dbRes) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('posting new task failed', err);
+            res.sendStatus(500);
+        })
+})
 
 
 
